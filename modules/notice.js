b630579463fc2799a29a0469d05aa111bfdfb078
i207M <1004168129@qq.com>
2021-03-14 23:41:20 +0800

    notice <p>

--- modules/notice.js
+++ modules/notice.js
@@ -1,23 +1,26 @@
 // Localize all elements with a data-i18n="message_name" attribute
-var localizedElements = document.querySelectorAll('[data-i18n]'), el, message;
+var localizedElements = document.querySelectorAll('[data-i18n]'), el, message
 for (var i = 0; i < localizedElements.length; i++) {
-    el = localizedElements[i];
-    message = chrome.i18n.getMessage(el.getAttribute('data-i18n'));
+    el = localizedElements[i]
+    message = chrome.i18n.getMessage(el.getAttribute('data-i18n'))
 
     // Capitalize first letter if element has attribute data-i18n-caps
     if (el.hasAttribute('data-i18n-caps')) {
-        message = message.charAt(0).toUpperCase() + message.substr(1);
+        message = message.charAt(0).toUpperCase() + message.substr(1)
     }
 
-    el.innerHTML = message;
+    el.innerHTML = message
 }
 
-background = chrome.extension.getBackgroundPage();
-tomatoImageEl = document.getElementById('tomato-img');
-tomatoImageEl.src = "../icons/" + background.getIconMode(background.mainPomodoro.nextMode) + "_full.png";
+background = chrome.extension.getBackgroundPage()
+tomatoImageEl = document.getElementById('tomato-img')
+noticeContentEl = document.getElementById('notice-content')
+
+var nextMode = background.mainPomodoro.nextMode
+tomatoImageEl.src = "../icons/" + background.getIconMode(nextMode) + "_full.png"
+noticeContentEl.innerHTML = chrome.i18n.getMessage("timer_end_notification_body",
+    chrome.i18n.getMessage(nextMode))
 
 chrome.windows.getCurrent(function (window) {
     chrome.windows.update(window.id, { drawAttention: true })
-});
-
-
+})
