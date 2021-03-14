a2817940ad5591ab5398c282c0a617ab77c7a483
i207M <1004168129@qq.com>
2021-03-14 12:26:14 +0800

    notice

---
+++ modules/notice.js
@@ -0,0 +1,13 @@
+// Localize all elements with a data-i18n="message_name" attribute
+var localizedElements = document.querySelectorAll('[data-i18n]'), el, message;
+for (var i = 0; i < localizedElements.length; i++) {
+    el = localizedElements[i];
+    message = chrome.i18n.getMessage(el.getAttribute('data-i18n'));
+
+    // Capitalize first letter if element has attribute data-i18n-caps
+    if (el.hasAttribute('data-i18n-caps')) {
+        message = message.charAt(0).toUpperCase() + message.substr(1);
+    }
+
+    el.innerHTML = message;
+}
